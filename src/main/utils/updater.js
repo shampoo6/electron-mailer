fb5844061a483e2f9902454e093838e6a03c822c
shampoo6 <shampoo6@163.com>
2020-08-27 10:51:20 +0800

    重写更新程序

--- src/main/utils/updater.js
+++ src/main/utils/updater.js
@@ -1,5 +1,6 @@
 import {dialog} from 'electron'
 import {autoUpdater} from 'electron-updater'
+import pjson from '../../package.json'
 
 export default () => {
   let message = {
@@ -8,7 +9,11 @@ export default () => {
     updateAva: '检测到新版本，正在下载……',
     updateNotAva: '现在使用的就是最新版本，不用更新'
   }
+  console.log('npm version is: ' + pjson.version)
+  const loadUrl = `https://github.com/shampoo6/electron-mailer/releases/download/v${pjson.version}/`
+  console.log(loadUrl)
   // const uploadUrl = 'http://61.4.184.177:7799/download/' // 下载地址，不加后面的**.exe
+  autoUpdater.setFeedURL(loadUrl)
   autoUpdater.on('error', () => {
     dialog.showErrorBox('错误', message.error)
   })
